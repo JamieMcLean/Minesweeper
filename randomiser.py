0c984e85be6f957533dc89943969fdaf9932e242
Edward-Rhodes <rhodes.e@etoncollege.org.uk>
2016-05-18 12:00:48 +0100

    Add files via upload
    This is the randomiser that puts the bombs into random places on the grid.

---
+++ randomiser.py
@@ -0,0 +1,8 @@
+import random
+
+def my_shuffle():
+        array = ['b.1','b.2','b.3','b.4','b.5','b.6']
+        random.shuffle(array)
+        shuffledArray = array
+        return shuffledArray
+my_shuffle()
