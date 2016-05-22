c0233e558823b06ed1974240e2d15af21e51a6fa
nswids <swidler.n@etoncollege.org.uk>
2016-05-22 13:41:36 +0100

    here is the first board, which is the baord that user will see

---
+++ proper board '+'.py
@@ -0,0 +1,16 @@
+
+import random
+
+
+def showGrid():
+    board= []
+    for i in range(9):
+        board.append([])
+        for x in range(9):
+            board[i].append('+')
+    for row in board:
+        print ("  ".join(row))
+
+
+
+showGrid()
