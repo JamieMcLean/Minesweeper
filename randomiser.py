f4d22cda56af61c6abb5f8ace9538dfe83f83dee
Adriendaa <alexandre.a@etoncollege.org.uk>
2016-05-23 10:39:54 +0100

    Delete randomiser.py

--- randomiser.py
+++
@@ -1,8 +0,0 @@
-import random
-
-def my_shuffle():
-        array = ['b.1','b.2','b.3','b.4','b.5','b.6']
-        random.shuffle(array)
-        shuffledArray = array
-        return shuffledArray
-my_shuffle()
